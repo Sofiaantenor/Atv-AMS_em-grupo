e416a1555857000b1224e358eaf1e19ce17a1682
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2021-10-27 18:02:54 -0300

    Add files via upload

---
+++ WhileCommand/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WhileCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+            int contador = 1;
+            int qtdPessoas = 0;
+
+           
+
+           while (contador <= 10)
+           {
+               contador++;
+               Console.WriteLine("Informe sua idade:");
+               idade = Convert.ToInt32(Console.ReadLine());
+           
+               if (idade >= 18)
+           {
+               qtdPessoas = qtdPessoas + 1;
+           }
+
+           }
+           Console.WriteLine($"Dentre essas pessoas, {qtdPessoas} têm permissão para dirigir!");
+
+        }
+    }
+}
